f0af1cd58092b3e14b088a6cb90156c0fe037a4f
Yurii Orlov <orlow.yurii@gmail.com>
2021-05-10 02:54:21 +0300

    Final Ready for validators

---
+++ HW-#10/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".swords").click(function(){
+      $(".firstsword").toggleClass("open1");
+    });
+  });
+  $(document).ready(function(){
+    $(".swords").click(function(){
+      $(".secondsword").toggleClass("open2");
+    });
+  });
+  $(document).ready(function(){
+    $(".swords").click(function(){
+      $(".thirdsword").toggleClass("open3");
+    });
+  });
+  $(document).ready(function(){
+    $(".swords").click(function(){
+      $(".vader").toggleClass("openvader");
+    });
+  });
+  $(document).ready(function(){
+    $(".balls").click(function(){
+      $(".newtonspendulum").toggleClass("skywalker");
+    });
+  });
